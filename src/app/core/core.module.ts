94dff46a653840dc9d7b97939b5f2a49042e065d
Vincent Fillon <vfillon@ics.fr>
2025-03-10 17:35:54 +0100

    Ajout des journées mondiales

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,9 +6,10 @@ import { AuthService } from './services/auth.service';
 import { MoodService } from './services/mood.service';
 import { NotificationService } from './services/notification.service';
 import { SocketService } from './services/socket.service';
+import { TicTacToeService } from './services/tic-tac-toe.service';
 import { UploadService } from './services/upload.service';
 import { UserService } from './services/user.service';
-import { TicTacToeService } from './services/tic-tac-toe.service';
+import { WorldDaysService } from './services/world-days.service';
 
 @NgModule({
   declarations: [],
@@ -22,6 +23,7 @@ import { TicTacToeService } from './services/tic-tac-toe.service';
     UploadService,
     NotificationService,
     TicTacToeService,
+    WorldDaysService,
   ],
 })
 export class CoreModule { }
